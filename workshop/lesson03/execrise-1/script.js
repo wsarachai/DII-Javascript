cb7cb65f46a4bd76dc6c7f281b6ca5194e19b04e
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2023-04-06 10:59:28 +0700

    Lesson03: execrise1 implementation

--- workshop/lesson03/execrise-1/script.js
+++ workshop/lesson03/execrise-1/script.js
@@ -1,17 +1,1 @@
 'use strict';
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
-
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
-};
-
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-  if (!guess) {
-  } else if (guess === secretNumber) {
-  }
-});
