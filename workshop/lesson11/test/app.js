f8d447ee9c1c38d02d4cd383e3a8de5a2acf4f0f
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2023-04-19 12:02:20 +0700

    lesson11 exercise-2

--- workshop/lesson11/test/app.js
+++ workshop/lesson11/test/app.js
@@ -1,13 +1,11 @@
 'use strict';
+
 const express = require('express');
 const tourRoute = require('./tours/tourRoutes');
 
 const app = express();
 app.use(express.json());
-
 app.use('/api/v1/tours', tourRoute);
+app.use(express.static(`${__dirname}/public`));
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}...`);
-});
+module.exports = app;
